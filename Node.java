06b3353f3c940fd33dad4bb259fb7178bf3585ce
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-09-12 22:54:03 -0300

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,10 @@
+// Classe Node para representar os nós da lista encadeada
+public class Node {
+    Elemento elemento;
+    Node proximo;
+
+    public Node(Elemento elemento) {
+        this.elemento = elemento;
+        this.proximo = null;
+    }
+}
